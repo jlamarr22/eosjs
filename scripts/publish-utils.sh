76db8de733eae088d7c40462c3d45c538cc90e37
Scott Arnette <scott.a.arnette@gmail.com>
2020-02-25 15:50:47 -0500

    Adjusting util script.

--- scripts/publish-utils.sh
+++ scripts/publish-utils.sh
@@ -4,15 +4,12 @@ setup_git() {
   # Set the user name and email to match the API token holder
   # This will make sure the git commits will have the correct photo
   # and the user gets the credit for a checkin
-  git config --global user.email "devops@block.one"
-  git config --global user.name "blockone-devops"
+  git config --global user.email "user@email.com"
+  git config --global user.name "username"
   git config --global push.default matching
   
   # Get the credentials from a file
   git config credential.helper "store --file=.git/credentials"
-  
-  # This associates the API Key with the account
-  echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
 }
 
 ensure_version_match() {
